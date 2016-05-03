852b87c6ca9d3497848e61fea06ac8c7d92660ab
Noora Al-Mana <nalmana.babson@gmail.com>
2016-05-03 18:36:45 -0400

    Inital commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function clickHandler(){
+	var element1 = document.getElementById('1');
+	var element2 = document.getElementById('2');
+	element2.innerHTML = element1.innerHTML; 
+}
+
+function handleClick() {
+	var element3 = document.getElementById('3');
+	var element4 = document.getElementById('4');
+	element4.innerHTML = "Right?!"
+}
